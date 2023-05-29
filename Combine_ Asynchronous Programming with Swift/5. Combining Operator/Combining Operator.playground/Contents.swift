8515a3aa6027a4c1d440c9ed504850ccd991b97a
Muhammad Ariful Islam <arif.mbstu22@gmail.com>
2023-05-29 06:32:45 +0600

    Combining operator added
    Combining operator added

---
+++ Combine_ Asynchronous Programming with Swift/5. Combining Operator/Combining Operator.playground/Contents.swift
@@ -0,0 +1,102 @@
+import UIKit
+import Combine
+
+public func example(of description: String,
+                    action: () -> Void) {
+    print("\n---- Example of:", description, "----")
+    action()
+}
+
+var subscriptions = Set<AnyCancellable>()
+
+example(of: "prepend(output...)") {
+    let publisher = [3,4].publisher
+    
+    publisher
+        .prepend(-1, 0)
+        .prepend(1, 2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "prepend(sequence)") {
+    let publisher = [5, 6, 7].publisher
+    
+    publisher
+        .prepend([3, 4])
+        .prepend(Set(1...2))
+        .prepend(stride(from: 6, to: 11, by: 2))
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "prepend(Publisher)") {
+    let publisher1 = [3, 4].publisher
+    let publisher2 = [1, 2].publisher
+    
+    publisher1
+        .prepend(publisher2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "prepend(Publisher) #2") {
+    let publisher1 = [3, 4].publisher
+    let publisher2 = PassthroughSubject<Int, Never>()
+    
+    publisher1
+        .prepend(publisher2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    publisher2.send(1)
+    publisher2.send(2)
+    
+    publisher2.send(completion: .finished)
+}
+
+example(of: "append(Output...)") {
+    let publisher = [1].publisher
+    
+    publisher
+        .append(2, 3)
+        .append(4)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "append(Output...) #2") {
+    let publisher = PassthroughSubject<Int, Never>()
+    
+    publisher
+        .append(3, 4)
+        .append(5)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    publisher.send(1)
+    publisher.send(2)
+    
+    publisher.send(completion: .finished)
+}
+
+example(of: "append(Sequence)") {
+    let publisher = [1, 2, 3].publisher
+    
+    publisher
+        .append([4, 5])
+        .append(Set([6, 7]))
+        .append(stride(from: 8, to: 11, by: 2))
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "append(Publisher)") {
+    let publisher1 = [1, 2].publisher
+    let publisher2 = [3, 4].publisher
+    
+    publisher1
+        .append(publisher2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
